392d5809854c05e3aa5aa30201fef06bc8a90107
Your Name <you@example.com>
2022-04-30 01:14:47 +0600

    register page updated

--- src/Pages/RegisterPage/Register.jsx
+++ src/Pages/RegisterPage/Register.jsx
@@ -1,36 +1,59 @@
+import { render } from "@testing-library/react";
 import React, { useState } from "react";
 import StudentRegister from "../../Components/RegisterForm/StudentRegister";
 import TeacherRegister from "../../Components/RegisterForm/TeacherRegister";
 import style from "./Register.module.css";
 const Register = () => {
-    const isTeacher = useState(false);
-    const isStudent = useState(false);
+    const [isTeacher, setIsTeacher] = useState(false);
+  const [isStudent, setIsStudent] = useState(false);
+  
+//   const handleButton = (role) => {
+// if (role==='student') {
+//   isStudent(true);
+// } else if(role==='teacher') {
+//   isTeacher(true);
+// }
+
+//     document.getElementById('teacher').style.display = "none";
+//     document.getElementById('student').style.display = "none";
+// }
+//   const handleStudentButton = () => {
+//     isStudent(false);
+// }
+
   return (
     <div className={style.register_wrap_bg}>
       <div className="text-white text-center">
         <h4>Register Here </h4>
         <br />
         <div
-          className={`btn btn-warning text-dark fw-bold `}
-          onClick={() => isTeacher(true)}
+          className={`btn btn-warning text-dark fw-bold ${
+            isStudent || isTeacher ? "d-none" : "d-inline"
+          }`}
+          onClick={() => setIsTeacher(true)}
+          id="teacher"
         >
           As a TEACHER
         </div>{" "}
         <br /> <br />
         <div
-          className={`btn btn-warning text-dark fw-bold `}
-          onClick={() => isStudent(true)}
+          className={`btn btn-warning text-dark fw-bold ${
+            isStudent || isTeacher ? "d-none" : "d-inline"
+          }`}
+          onClick={() => setIsStudent(true)}
+          id="student"
         >
           As a STUDENT
         </div>
       </div>
-      {/* ${isTeacher || isStudent ? style.register_btn : style.register_btn_hide} */}
-      <div className={`teacherForm`}>
-        <TeacherRegister></TeacherRegister>
-      </div>
-      <div className={`studentForm`}>
-        {/* <StudentRegister></StudentRegister> */}
-      </div>
+
+      {
+        isTeacher? <TeacherRegister/>:<div></div>
+     }
+      {
+        isStudent? <StudentRegister/>:<div></div>
+     }
+      
     </div>
   );
 };
